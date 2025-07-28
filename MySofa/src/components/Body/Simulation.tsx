8bdc40d9fbf64e1020067b1f43c8b5c880bd9c19
udune <udune@naver.com>
2025-07-28 23:11:54 +0900

    시뮬레이션 버튼 연결 수정

--- MySofa/src/components/Body/Simulation.tsx
+++ MySofa/src/components/Body/Simulation.tsx
@@ -3,9 +3,13 @@ import useMyItem from "../../hook/useMyitem";
 import useTitle from "../../hook/useTitle";
 
 import { useParams } from "react-router-dom";
-import { CustomizationData, Product } from "@/types";
+import { Product } from "@/types";
+import { useAuth } from "@/contexts/AuthContext";
+import { customSessionService } from "@/services/customSessionService";
+import { CustomSessionRequest } from "@/types/customSession";
 
 const Simulation: React.FC = () => {
+  const { isAuthenticated, user } = useAuth();
   const params = useParams<{id: string}>();
   useTitle("MySofa :: 제품 상세 페이지");
 
@@ -14,19 +18,34 @@ const Simulation: React.FC = () => {
     return <div>데이터 로딩중</div>;
   }
 
-  const onSubmit = (item: Product): void => {
-    // 시뮬레이션 유니티 WebGL 연결
-    const customizationData: CustomizationData = {
-      token: "abcdef",
-      ...item,
-    };
-    
-    localStorage.setItem(
-      "mysofa_customdata",
-      JSON.stringify(customizationData)
-    );
+  const onSubmit = async (item: Product): Promise<void> => {
+    if (!isAuthenticated()) {
+      console.log('로그인이 필요합니다.')
+      return;
+    }
 
-    window.location.href = "https://unity.my-sofa.org/";
+    if (!user?.id) {
+      console.log('user.id가 없습니다.', user)
+    }
+    
+    try {
+      const sessionRequest: CustomSessionRequest = {
+        user_id: user?.id,
+        product_id: item.id,
+        name: item.name,
+        custom_name: item.custom_name,
+        color: item.color,
+        material: item.material,
+        size: item.size,
+        model_type: item.model_type,
+      }
+
+      const sessionResponse = await customSessionService.createCustomSession(sessionRequest);
+
+      window.location.href = `https://unity.my-sofa.org/?sessionId=${sessionResponse.id}&isEdit=true`;
+    } catch (error) {
+      console.log('커스텀 세션 생성 실패', error);
+    }
   };
 
   return (
